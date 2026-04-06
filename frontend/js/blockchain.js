b4bbdb04c66f1c9160dbc56d992850ccaaee745f
Ketaki Bhoir <bhoir.ketaki.anil@gmail.com>
2026-04-06 18:24:47 +0530

    Initial commit

--- frontend/js/blockchain.js
+++ frontend/js/blockchain.js
@@ -2,20 +2,114 @@ let web3;
 let contract;
 let account;
 
-const contractAddress = "PASTE_CONTRACT_ADDRESS";
-const abi = [PASTE_ABI_HERE];
+const contractAddress = "0x8f8421e683956Ad15b6E7d301182D3D79E2A46C9";
+
+const contractABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "records",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "student",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "subject",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "date",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "present",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_student",
+        "type": "address"
+      },
+      {
+        "internalType": "string",
+        "name": "_subject",
+        "type": "string"
+      },
+      {
+        "internalType": "bool",
+        "name": "_present",
+        "type": "bool"
+      }
+    ],
+    "name": "markAttendance",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getRecords",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "address",
+            "name": "student",
+            "type": "address"
+          },
+          {
+            "internalType": "string",
+            "name": "subject",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "date",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "present",
+            "type": "bool"
+          }
+        ],
+        "internalType": "struct Attendance.Record[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  }
+];
 
 async function connectWallet() {
   if (window.ethereum) {
     web3 = new Web3(window.ethereum);
+    await window.ethereum.request({ method: "eth_requestAccounts" });
 
-    const accounts = await window.ethereum.request({
-      method: "eth_requestAccounts",
-    });
-
+    const accounts = await web3.eth.getAccounts();
     account = accounts[0];
 
-    contract = new web3.eth.Contract(abi, contractAddress);
+    contract = new web3.eth.Contract(contractABI, contractAddress);
 
     console.log("Connected:", account);
   } else {

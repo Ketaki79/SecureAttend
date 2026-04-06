aa2b0bfd594b4d5d2a4601c1deeed962b4d8fdd6
Ketaki Bhoir <bhoir.ketaki.anil@gmail.com>
2026-04-06 16:57:34 +0530

    Initial commit

--- frontend/js/blockchain.js
+++ frontend/js/blockchain.js
@@ -2,23 +2,23 @@ let web3;
 let account;
 
 async function connectWallet() {
-  if (window.ethereum) {
+  if (typeof window.ethereum !== "undefined") {
     try {
-      web3 = new Web3(window.ethereum);
-
       const accounts = await window.ethereum.request({
         method: "eth_requestAccounts",
       });
 
+      web3 = new Web3(window.ethereum);
       account = accounts[0];
+
       console.log("Connected:", account);
       alert("Wallet Connected: " + account);
 
     } catch (err) {
-      console.error(err);
-      alert("MetaMask access denied");
+      console.error("MetaMask Error:", err);
+      alert("User denied MetaMask access");
     }
   } else {
-    alert("Please install MetaMask");
+    alert("MetaMask not installed");
   }
 }

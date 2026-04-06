46cfdc29624e8b93ddac10cf93a985e5256e4abb
Ketaki Bhoir <bhoir.ketaki.anil@gmail.com>
2026-04-06 18:19:11 +0530

    Initial commit

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545, 
+      network_id: "5777",
+    },
+  },
+
+  compilers: {
+    solc: {
+      version: "0.8.21", 
+      settings: {
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "paris" 
+      }
+    }
+  }
+};
